8db592726e429726bc36b29cf2952e6f56a3238f
misaki1874 <931205649@qq.com>
2021-05-08 23:53:05 +0800

    new file

---
+++ frontend/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import User
+# Create your views here.
+
+def GET_test(request):
+    # 渲染这个页面 并返回到前端
+    return render(request,'GET_test.html')
+
+def POST_test(request):
+    return render(request,'POST_test.html')
+
+def POST(request):
+    userName=request.POST['userName']
+    password=request.POST['password']
+    mail=request.POST['mail']
+    port=request.POST['port']
+    User.objects.create(user_name=userName,user_code=password,user_email=mail,smtp_state='1',pop_state='1',port=port)
+    return render(request,'result.html',context={'data':userName})
+
+def GET_test(request):
+    data = User.objects.all()
+    # context必须是一个键值对
+    return render(request,'GET_test.html',context={'data':data})
