9c0072762f89626d341cc1b2dac8c5ec49e7d2fd
ysbm1485 <842304003@qq.com>
2021-05-12 14:36:27 +0800

    修改登陆注册，存储登陆状态，加权限验证‘

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,8 @@
 import pymysql
 from django.http import JsonResponse
 from django.shortcuts import render
+
+from . import models
 from .models import User
 # Create your views here.
 
@@ -17,68 +19,89 @@ def connectdb():
     con = pymysql.connect(**config)
     return con
 
-# 登录页面
-def Login(request):
-    return render(request,'Login.html')
-
-# 登录认证
-def user_identified(request):
-    email = request.POST.get('email',None)
-    password = request.POST.get('password',None)
-    con = connectdb()
-    cursor = con.cursor()
-    sql = "SELECT COUNT(*) FROM user where user_email = '%s'"
-    data = (email,)
-    cursor.execute(sql%data)
-    result = cursor.fetchone()
-    userNum = result[0]
-    if userNum == 0:  # 无记录 不存在该用户
-        return JsonResponse({"message": "用户不存在，请进行注册"})
-
-    sql = "SELECT user_code FROM user where user_email = '%s'"
-    cursor.execute(sql%data)
-    result = cursor.fetchone()
-    code = result[0]
-    cursor.close()
-    con.close()
-    # 密码验证
-    if code == password:
-        return JsonResponse({"message": "登陆成功"})
-    else:
-        return JsonResponse({"message": "邮箱或密码输入错误"})
-
-
 # 注册页面
 def SignUp(request):
     return render(request,'SignUp.html')
 
-# 用户注册
-def register(request):
-    username = request.POST.get('user',None)
-    password = request.POST.get('password',None)
-    useremail = username+'@skyfall.icu'
-    con = connectdb()
-    cursor = con.cursor()
-    sql = "SELECT COUNT(*) FROM user where user_name = '%s'"
-    data = (username,)
-    cursor.execute(sql%data)
-    result = cursor.fetchone()
-    userNum = result[0]
-    if userNum > 0:  # 用户已存在
-        return JsonResponse({"message": "用户名已被占用"})
-
-    sql = "INSERT INTO user(user_name,user_code,user_email,smtp_state,pop_state) VALUES('%s','%s','%s','%s','%s')"
-    data = (username,password,useremail,"1","1")  # 默认可以收发邮件
-    cursor.execute(sql%data)
-    con.commit()
-    cursor.close()
-    con.close()
-    return JsonResponse({"message": "注册成功"})
-
+# 登录页面
+def Login(request):
+    return render(request,'Login.html')
 
 #主页
 def Index(request):
     return render(request,'Index.html')
+
+# 写邮件页面
+def WriteEmail(request):
+    return render(request,'WriteEmail.html')
+
+# 收件箱页面
+def ReceiveEmail(request):
+    return render(request,'ReceiveEmail.html')
+
+# 已发送页面
+def SentEmail(request):
+    return render(request,'SentEmail.html')
+
+# 修改密码页面
+def ChangePass(request):
+    return render(request,'ChangePass.html')
+
+# 用户注册
+# 参数：用户名，密码
+# 注册成功，返回消息和200状态码
+# 注册失败，返回消息和404状态码
+def register(request):
+    # 登录状态不允许注册
+    if request.session.get('isLogin', None):
+        return JsonResponse({"message": "登录状态，无法注册", "status": 404})
+
+    username = request.POST.get('username',None)
+    password = request.POST.get('password',None)
+    useremail = username+'@skyfall.icu'
+    sameNameUser = models.User.objects.get(user_name=username)
+    # 用户已存在
+    if sameNameUser or username == 'admin':
+        return JsonResponse({"message": "该用户名已被占用，请重新输入用户名", "status": 404})
+    # 注册新用户
+    models.User.objects.create(
+        user_name=username,
+        user_code=password,
+        user_email=useremail,
+        smtp_state='1',
+        pop_state='1'
+    )
+    return JsonResponse({"message": "注册成功", "status": 200})
+
+
+# 登录认证
+# 参数：用户名, 密码
+# 管理员权限No=1，普通用户权限No=0
+def user_identified(request):
+    # 若已经登录，直接进入已登录账号
+    if request.session.get('isLogin', None):
+        return JsonResponse({"message": "你已经登录", "status": 404})
+
+    username = request.POST.get('username',None)
+    password = request.POST.get('password',None)
+    user = models.User.objects.get(user_name=username)
+    if not user.exists():  # 无记录 不存在该用户
+        return JsonResponse({"message": "用户不存在，请进行注册", "status": 404})
+    # 身份验证
+    if username == 'admin' and password == '123456':  # 管理员
+        # 设置登录状态为True，设置登录id为username
+        request.session['isLogin'] = True
+        request.session['userId'] = username
+        return JsonResponse({"message": "登陆成功", "status": 200, "userId":username, "authorityNo": 1})
+    elif user.user_code == password:  # 普通用户
+        request.session['isLogin'] = True
+        request.session['username'] = user.user_name
+        return JsonResponse({"message": "登陆成功", "status": 200, "userId":username, "authorityNo": 0})
+    else:
+        return JsonResponse({"message": "用户名或密码输入错误", "status": 404})
+
+
+
 
 def GET_test(request):
     return render(request,'GET_test.html')
